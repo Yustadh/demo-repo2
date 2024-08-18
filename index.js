b8605f247ac1d31bc98fd1b88c21977ff8f5896b
Yusuf-Sanni <sanniyusuf9394@gmail.com>
2024-08-18 12:16:13 +0100

    I commited my third

--- index.js
+++ index.js
@@ -1,126 +1,126 @@
 
-//Program to calculate the circunference and area of a circle
+// Program to calculate the circunference and area of a circle
 
-// let radius;
-// const PI= 3.14159;
-// let circumference;
-// let area;
-// const myButton2=document.getElementById('evaluate');
-// myButton2.onclick=function(){
-//     radius= document.getElementById('radius').value;
-//     radius = Number(radius);
-//     circumference = 2*PI*radius;
-//     area = PI*(radius^2)
-//     document.getElementById('textArea').textContent=`The circumference of the circle is ${circumference}cm and its area is ${area}cm^2`
-// }
+let radius;
+const PI= 3.14159;
+let circumference;
+let area;
+const myButton2=document.getElementById('evaluate');
+myButton2.onclick=function(){
+    radius= document.getElementById('radius').value;
+    radius = Number(radius);
+    circumference = 2*PI*radius;
+    area = PI*(radius^2)
+    document.getElementById('textArea').textContent=`The circumference of the circle is ${circumference}cm and its area is ${area}cm^2`
+}
 
 // counter programs begins here
-// let counter=0;
-// let Changeme;
-// const increaseBtn= document.getElementById('increase');
-// const decreaseBtn = document.getElementById('decrease');
-// const resetBtn=document.getElementById('reset');
-// increaseBtn.onclick= function(){
-//     let increase=counter++;
-//     document.getElementById('Changeme').textContent=increase
+let counter=0;
+let Changeme;
+const increaseBtn= document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const resetBtn=document.getElementById('reset');
+increaseBtn.onclick= function(){
+    let increase=counter++;
+    document.getElementById('Changeme').textContent=increase
 
-// }
-// decreaseBtn.onclick= function(){
-//     let decrease=counter--;
-//     document.getElementById('Changeme').textContent=decrease
+}
+decreaseBtn.onclick= function(){
+    let decrease=counter--;
+    document.getElementById('Changeme').textContent=decrease
 
-// }
-// resetBtn.onclick= function(){
-//     counter=0
-//     document.getElementById('Changeme').textContent=counter;
+}
+resetBtn.onclick= function(){
+    counter=0
+    document.getElementById('Changeme').textContent=counter;
 
-// }
+}
 
 // Random number generator program starts here
-// let minNumber=10;
-// let maxNumber=100;
-// let randomNumber1;
-// let randomNumber2;
-// let randomNumber3;
+let minNumber=10;
+let maxNumber=100;
+let randomNumber1;
+let randomNumber2;
+let randomNumber3;
 
-// const generate= document.getElementById('generate');
-// generate.onclick=function(){
-//     randomNumber1=Math.floor(Math.random()*(maxNumber-minNumber)+1) +minNumber ;
-//     console.log(randomNumber1);
-//      randomNumber2=Math.floor(Math.random()*(maxNumber-minNumber)+1) +minNumber ;
-//     console.log(randomNumber2);
-//      randomNumber3=Math.floor(Math.random()*(maxNumber-minNumber)+1) +minNumber ;
-//     console.log(randomNumber3);
+const generate= document.getElementById('generate');
+generate.onclick=function(){
+    randomNumber1=Math.floor(Math.random()*(maxNumber-minNumber)+1) +minNumber ;
+    console.log(randomNumber1);
+     randomNumber2=Math.floor(Math.random()*(maxNumber-minNumber)+1) +minNumber ;
+    console.log(randomNumber2);
+     randomNumber3=Math.floor(Math.random()*(maxNumber-minNumber)+1) +minNumber ;
+    console.log(randomNumber3);
 
-//     document.getElementById('rytHere').textContent=randomNumber1;
-//     document.getElementById('rytThere').textContent=randomNumber2;
-//     document.getElementById('rytNow').textContent=randomNumber3;
-// }
+    document.getElementById('rytHere').textContent=randomNumber1;
+    document.getElementById('rytThere').textContent=randomNumber2;
+    document.getElementById('rytNow').textContent=randomNumber3;
+}
 
 // Working with forms program starts here
 
-// const Subscribe= document.getElementById('subscribe');
-// const visa = document.getElementById('visa');
-// const masterCard = document.getElementById('MasterCard');
-// const payPal = document.getElementById('paypal');
-// let displayed =document.getElementById('Go');
-// let subStatus= document.getElementById('Status');
-// const pushMe = document.getElementById('pushme');
+const Subscribe= document.getElementById('subscribe');
+const visa = document.getElementById('visa');
+const masterCard = document.getElementById('MasterCard');
+const payPal = document.getElementById('paypal');
+let displayed =document.getElementById('Go');
+let subStatus= document.getElementById('Status');
+const pushMe = document.getElementById('pushme');
 
-// pushMe.onclick= function(){
+pushMe.onclick= function(){
      
-//     if(Subscribe.checked){
-//         displayed.textContent=`You subscribed`;
-//     }else{
-//          displayed.textContent=`Please subscribe`;
-//     }
-//     if(visa.checked){
-//          subStatus.textContent= `You payed with Visa`
-//     }else if(masterCard.checked){
-//           subStatus.textContent=`You payed with Master card`
-//     }else if(payPal.checked){
-//           subStatus.textContent=`You payed with Paypal`
-//     }else{
-//           subStatus.textContent=`Please pick a payment method`
-//     }
+    if(Subscribe.checked){
+        displayed.textContent=`You subscribed`;
+    }else{
+         displayed.textContent=`Please subscribe`;
+    }
+    if(visa.checked){
+         subStatus.textContent= `You payed with Visa`
+    }else if(masterCard.checked){
+          subStatus.textContent=`You payed with Master card`
+    }else if(payPal.checked){
+          subStatus.textContent=`You payed with Paypal`
+    }else{
+          subStatus.textContent=`Please pick a payment method`
+    }
 
 
-// }
+}
 
 // Temperature conversion program starts here
 
-// const TempValue=document.getElementById('tempValue');
-// const CF= document.getElementById('Celsius-Fahrenheit');
-// const FC = document.getElementById('Fahrenheit-Celsius');
-// const Display= document.getElementById('display-conversion');
-// let temp;
+const TempValue=document.getElementById('tempValue');
+const CF= document.getElementById('Celsius-Fahrenheit');
+const FC = document.getElementById('Fahrenheit-Celsius');
+const Display= document.getElementById('display-conversion');
+let temp;
 
 
-//  function Con(){
-//      if(CF.checked){
-//        temp=Number(TempValue.value);
-//        temp= temp *9/5 +32;
-//        Display.textContent= temp.toFixed(1)+ "℉"
-//      }else if(FC.checked){
-//       temp=Number(TempValue.value);
-//       temp= temp - 32 *5/9;
-//       Display.textContent=temp;
-//      }else{
-//       Display.textContent= `Please click on a button`
-//      }
+ function Con(){
+     if(CF.checked){
+       temp=Number(TempValue.value);
+       temp= temp *9/5 +32;
+       Display.textContent= temp.toFixed(1)+ "℉"
+     }else if(FC.checked){
+      temp=Number(TempValue.value);
+      temp= temp - 32 *5/9;
+      Display.textContent=temp;
+     }else{
+      Display.textContent= `Please click on a button`
+     }
      
-// }
+}
 
 // Looking at callback function
 // example
-// function Bye(callback){{
-//   setTimeout(function(){
-//     console.log('YOu')
-//     callback()
-//   },3000)
-// }
+function Bye(callback){{
+  setTimeout(function(){
+    console.log('YOu')
+    callback()
+  },3000)
+}
 
-// }
+}
 function Good(){
   console.log('and me')
 
@@ -179,7 +179,7 @@ function changedArray(element){
   return `${results[2]} ${results[0]} ${results[1]}`
 }
 
-constructors
+
 
 
 
